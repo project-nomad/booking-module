43b6ef6d9a0ca986ce954afd87e9ce24bb59d608
Justin Shih <justinshih@Justins-MacBook-Pro.local>
2018-06-19 17:04:37 -0700

    Adds closeconnection to dailyprices iteration

--- sample-data/postgres-insert.js
+++ sample-data/postgres-insert.js
@@ -1,5 +1,10 @@
 const { listings, reservations, dailyPrices } = require('./generator');
-const { insertListing, insertReservation, insertPrice } = require('./insert-helpers');
+const {
+  insertListing,
+  insertReservation,
+  insertPrice,
+  closeConnection,
+} = require('./insert-helpers');
 
 // insert into Postgres database
 listings.forEach(listing => insertListing(listing));
@@ -8,9 +13,13 @@ console.log(`${listings.length} listings loaded!`);
 reservations.forEach(reservation => insertReservation(reservation));
 console.log(`${reservations.length} reservations loaded!`);
 
-dailyPrices.forEach(price => insertPrice(price));
+dailyPrices.forEach((price, index) => {
+  insertPrice(price);
+  if (index === dailyPrices.length - 1) {
+    closeConnection();
+  }
+});
 console.log(`${dailyPrices.length} daily prices loaded!`);
-
 console.log('all sample data generated');
 
 // setTimeout(insertHelpers.closeConnection, 3000);
