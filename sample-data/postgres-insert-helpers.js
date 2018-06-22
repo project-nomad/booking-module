ca8582bec6397501011604924cfbda2ea3c08198
Justin Shih <justinshih@Justins-MacBook-Pro.local>
2018-06-21 19:04:09 -0700

    Creates and writes csv file for each table

--- sample-data/postgres-insert-helpers.js
+++ sample-data/postgres-insert-helpers.js
@@ -1,81 +1,65 @@
 const { Client } = require('pg');
+const fs = require('fs');
+const csv = require('fast-csv');
+const now = require('performance-now');
+
+// const csvStream = csv.createWriteStream({ headers: false });
+// const listingStream = fs.createWriteStream('./listings.csv', { flags: 'a' });
+// const reservationStream = fs.createWriteStream('./reservations.csv');
+// const dailyPricesStream = fs.createWriteStream('./dailyPrices.csv');
+
 const {
-  listings,
-  reservations,
-  dailyPrices,
-  t0,
-  now,
+  generateListing,
+  generateReservation,
+  generateDailyPrices,
 } = require('./generator');
 
-const config = {
-  host: 'localhost',
-  database: 'project_nomad_booking',
-  user: 'justinshih',
-  password: '',
-  port: 5432,
-};
+const numOfRecords = 10000000;
+// adds listings to csv
 
-const client = new Client(config);
-client.connect();
+fs.writeFileSync('./listings.csv', '');
+const t0 = now();
+for (let i = 0; i < 100; i += 1) {
+  let listings = [];
+  for (let j = 0; j < 100000; j += 1) {
+    const listing = generateListing();
+    listings.push(listing);
+  }
+  listings = listings.join('\n');
+  listings += '\n';
+  fs.appendFileSync('./listings.csv', listings);
+  if (i === 99) {
+    console.log(((now() - t0) / 1000), 'seconds');
+  }
+}
 
-// module.exports = {
-const insertListing = (listing, index) => {
-  const insertListingQuery = `INSERT INTO listings (id, avg_rating, review_count, max_adults, max_children,
-      max_infants, cleaning_fee, service_fee_perc, occ_tax_rate_perc, additional_guest_fee)
-      VALUES (${listing[0]}, ${listing[1]}, ${listing[2]}, ${listing[3]},
-        ${listing[4]}, ${listing[5]}, ${listing[6]}, ${listing[7]}, ${listing[8]}, ${listing[9]});`;
+//add reservations to csv
+fs.writeFileSync('./reservations.csv', '');
 
-  client.query(insertListingQuery, (err) => {
-    if (err) {
-      console.log(err);
-    }
-    if (index === listings.length - 1) {
-      console.log(`${listings.length} listings loaded!`);
-    }
-  });
-};
+// let reservations = [];
+for (let i = 0; i < 10000000; i += 1) {
+  let reservation = generateReservation(i);
+  reservation = reservation.join('\n');
+  reservations.push(reservation);
+  if (i % 100000 === 0) {
+    reservations = reservations.join('\n');
+    reservations += '\n';
+    fs.appendFileSync('./reservations.csv', reservations);
+    reservations = [];
+  }
+}
 
-const insertReservation = (reservation, index) => {
-  const insertReservationQuery = `INSERT INTO reservations (id, listing_id, begin_date, end_date)
-  VALUES (${reservation[0]}, ${reservation[1]}, TO_DATE('${reservation[2]}', 'MM/DD/YYYY'), TO_DATE('${reservation[3]}', 'MM/DD/YYYY'));`;
-
-  client.query(insertReservationQuery, (err) => {
-    if (err) {
-      console.log(err);
-    }
-    if (index === reservations.length - 1) {
-      console.log(`${reservations.length} reservations loaded!`);
-    }
-  });
-};
-
-const insertPrice = (price, index) => {
-  const insertPriceQuery = `INSERT INTO listing_daily_prices (id, listing_id, cost_per_night, begin_date)
-    VALUES (${price[0]}, ${price[1]}, ${price[2]}, TO_DATE('${price[3]}', 'MM/DD/YYYY'));`;
-
-  client.query(insertPriceQuery, (err) => {
-    if (err) {
-      console.log(err);
-    }
-    if (index === dailyPrices.length - 1) {
-      console.log(`${dailyPrices.length} daily prices loaded!`);
-      const t1 = now();
-      console.log('data seeded in', ((t1 - t0).toFixed(3) / 1000), `seconds for ${listings.length + reservations.length + dailyPrices.length} records`);
-      client.end();
-    }
-  });
-};
-
-// insert into Postgres database
-listings.forEach((listing, index) => {
-  insertListing(listing, index);
-});
-
-reservations.forEach((reservation, index) => {
-  insertReservation(reservation, index);
-});
-
-dailyPrices.forEach((price, index) => {
-  insertPrice(price, index);
-});
-
+// add dailyPrices to csv
+fs.writeFileSync('./dailyPrices.csv', '');
+let dailyPrices = [];
+for (let i = 0; i < 10000000; i += 1) {
+  let dailyPrice = generateDailyPrices(i);
+  dailyPrice = dailyPrice.join('\n');
+  dailyPrices.push(dailyPrice);
+  if (i % 100000 === 0) {
+    dailyPrices = dailyPrices.join('\n');
+    dailyPrices += '\n';
+    fs.appendFileSync('./dailyPrices.csv', dailyPrices);
+    dailyPrices = [];
+  }
+}
