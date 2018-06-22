2788bfaf72888c1e7d1993e93730021e8182c320
Justin Shih <justinshih@Justins-MacBook-Pro.local>
2018-06-21 19:02:23 -0700

    Adds COPY query for each table and csv

--- sample-data/schema.sql
+++ sample-data/schema.sql
@@ -7,7 +7,7 @@ CREATE DATABASE project_nomad_booking;
 \c project_nomad_booking;
 
 CREATE TABLE listings (
-  id int PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   avg_rating decimal (3, 2),
   review_count int NOT NULL,
   max_adults int NOT NULL,
@@ -20,15 +20,19 @@ CREATE TABLE listings (
 );
 
 CREATE TABLE reservations (
-  id int PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   listing_id int NOT NULL,
   begin_date date NOT NULL,
   end_date date NOT NULL
 );
 
 CREATE TABLE listing_daily_prices (
-  id int PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   listing_id int NOT NULL,
   cost_per_night decimal(10, 2) NOT NULL,
   begin_date date NOT NULL
 );
+
+COPY listings (avg_rating,review_count,max_adults,max_children,max_infants,cleaning_fee,service_fee_perc,occ_tax_rate_perc,additional_guest_fee) from '/Users/justinshih/HackReactor/airJordans/booking-module/listings.csv' DELIMITER ',' csv;
+COPY reservations (listing_id,begin_date,end_date) from '/Users/justinshih/HackReactor/airJordans/booking-module/reservations.csv' DELIMITER ',' csv;
+COPY listing_daily_prices (listing_id,cost_per_night,begin_date) from '/Users/justinshih/HackReactor/airJordans/booking-module/dailyPrices.csv' DELIMITER ',' csv;
