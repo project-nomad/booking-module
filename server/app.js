f0734af2f54ca55e0cdd7e968d6d963b3f667251
Justin Shih <justinshih@Justins-MacBook-Pro.local>
2018-06-23 17:47:22 -0700

    Adds CRUD endpoints for server

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
-const db = require('../database/index.js');
+const db = require('../database/index-postgres');
 
 const app = express();
 
-app.use('/', express.static(path.join(__dirname, '/../public')));
+app.use(express.static(path.join(__dirname, '/../public')));
 app.use('/listings/:listingId', express.static(path.join(__dirname, '/../public')));
 
 app.get('/listings/:listingId/booking/core', (req, res) => {
-  db.getCoreData(req.params.listingId, (err, results) => {
+  db.getBaseDataForListing(req.params.listingId, (err, results) => {
     if (err) {
       res.send(err);
     } else {
@@ -18,21 +18,22 @@ app.get('/listings/:listingId/booking/core', (req, res) => {
   });
 });
 
-// structure: http://localhost:3001/booking/availability/listingid/3?start_date=2018-07-01&end_date=2018-09-28
-app.get('/listings/:listingId/booking/availability', (req, res) => {
-  db.getReservationData(req.params.listingId, req.query.start_date, req.query.end_date, (err, results) => {
+// structure: http://localhost:3001/listings/3/booking/availability/?start_date=2018-07-01&end_date=2018-09-28
+app.get('/listings/:listingId/booking/availability/', (req, res) => {
+  db.getReservationDataForDateRange(req.params.listingId, req.query.begin_date, req.query.end_date, (err, results) => {
     if (err) {
       res.send(err);
     } else {
       res.header('Access-Control-Allow-Origin', '*');
+      console.log(results);
       res.status(200).send(results);
     }
   });
 });
 
-// structure: http://localhost:3001/booking/pricing/listingid/3?start_date=2018-07-01&end_date=2018-09-28
+// structure: http://localhost:3001/listings/3/booking/pricing/?end_date=2018-09-28
 app.get('listings/:listingId/booking/pricing/', (req, res) => {
-  db.getPricingData(req.params.listingId, req.query.start_date, req.query.end_date, (err, results) => {
+  db.getPricingDataForDateRange(req.params.listingId, req.query.end_date, (err, results) => {
     if (err) {
       res.send(err);
     } else {
@@ -43,7 +44,7 @@ app.get('listings/:listingId/booking/pricing/', (req, res) => {
 });
 
 // add reservation
-app.post('/reservations/:reservationId/', (req, res) => {
+app.post('/reservations/:listingId/', (req, res) => {
 
 });
 // update reservation
