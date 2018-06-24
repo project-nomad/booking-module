274d75144e334e258eac494e2a8f883c39f8c461
Justin Shih <justinshih@Justins-MacBook-Pro.local>
2018-06-23 17:42:07 -0700

    Resolves js and jsx files

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: path.join(__dirname, '/client/index.jsx'),
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
+        include: /client/,
         loader: 'babel-loader',
         options: {
           presets: ['env', 'react'],
