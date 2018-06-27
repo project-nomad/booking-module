f9c2c5d153c80689e58afd1406b4773a3c9fcc41
Justin Shih <justinshih@Justins-MacBook-Pro.local>
2018-06-27 09:11:02 -0700

    Adds loops to write rows to csv for cassandra

---
+++ sample-data/cass-insert-helpers.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const { generateListing, generateReservationCass, generateDailyPricesCass } = require('./generator');
+
+// adds listings to csv
+
+fs.writeFileSync('./listings-Cass.csv', '');
+
+let listings = [];
+for (let i = 1; i <= 10000000; i += 1) {
+  const listing = generateListing();
+  const prices = generateDailyPricesCass(i);
+  prices.map(elem => elem.push(...listing));
+  listings.push(...prices);
+  if (i % 100000 === 0) {
+    listings = listings.join('\n');
+    listings += '\n';
+    fs.appendFileSync('./listings-Cass.csv', listings);
+    listings = [];
+  }
+}
+
+// fs.writeFileSync('./reservations-Cass.csv', '');
+
+// let reservations = [];
+// for (let i = 1; i <= 10000000; i += 1) {
+//   let reservation = generateReservationCass(i);
+//   reservation = reservation.join('\n');
+//   reservations.push(reservation);
+//   if (i % 100000 === 0) {
+//     reservations = reservations.join('\n');
+//     reservations += '\n';
+//     fs.appendFileSync('./reservations-Cass.csv', reservations);
+//     reservations = [];
+//   }
+// }
