20764e0ecec580a1748aba6ab0d6dbdf88be81d2
Justin Shih <justinshih@Justins-MacBook-Pro.local>
2018-06-27 09:09:28 -0700

    Adds modified functions for cassandra schema

--- sample-data/generator.js
+++ sample-data/generator.js
@@ -1,3 +1,4 @@
+const uuid = require('uuid/v1');
 // helpers
 
 const getRandomInt = function getRandomIntegerBetweenValues(min, max) {
@@ -20,6 +21,13 @@ const getDateString = function getDateStringForSQLInsertion(date) {
   const year = date.getFullYear();
 
   return `${month}/${day}/${year}`;
+};
+const getDateStringCass = function getDateStringForSQLInsertion(date) {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  return `${year}-${month}-${day}`;
 };
 
 
@@ -48,7 +56,24 @@ const generateReservation = (i) => {
     const endOfReservation = new Date(nextReservation);
     endOfReservation.setDate(nextReservation.getDate() + getRandomInt(1, 10));
 
-    const row = [i + 1, getDateString(nextReservation), getDateString(endOfReservation)];
+    const row = [i, getDateString(nextReservation), getDateString(endOfReservation)];
+    reservations.push(row);
+    // write row;
+    nextReservation.setDate(endOfReservation.getDate() + getRandomInt(0, 10));
+  }
+  return reservations;
+};
+
+const generateReservationCass = (i) => {
+  const startDate = new Date(2018, 5, 15);
+  const nextReservation = new Date(startDate);
+  nextReservation.setDate(startDate.getDate() + getRandomInt(0, 10));
+  const reservations = [];
+  for (let j = 0; j < getRandomInt(5, 15); j += 1) {
+    const endOfReservation = new Date(nextReservation);
+    endOfReservation.setDate(nextReservation.getDate() + getRandomInt(1, 10));
+
+    const row = [i, getDateStringCass(nextReservation), getDateStringCass(endOfReservation), uuid()];
     reservations.push(row);
     // write row;
     nextReservation.setDate(endOfReservation.getDate() + getRandomInt(0, 10));
@@ -75,8 +100,27 @@ const generateDailyPrices = (i) => {
   return prices;
 };
 
+const generateDailyPricesCass = (i) => {
+  const priceStartDate = new Date(2018, 5, 1);
+  const nextDate = new Date(priceStartDate);
+  let nextPrice = getRandomInt(45, 500);
+  const prices = [];
+  for (let j = 0; j < getRandomInt(3, 8); j += 1) {
+    const row = [i, nextPrice, getDateStringCass(nextDate)];
+    // listing_id, cost_per_night, start_date
+
+    prices.push(row);
+    const potentialNextPrice = nextPrice + (getRandomInt(0, 50) * getRandomPosNeg());
+    nextPrice = potentialNextPrice > 45 ? potentialNextPrice : 45;
+    nextDate.setDate(nextDate.getDate() + getRandomInt(10, 50));
+  }
+  return prices;
+};
+
 module.exports = {
   generateListing,
   generateReservation,
   generateDailyPrices,
+  generateReservationCass,
+  generateDailyPricesCass,
 };
