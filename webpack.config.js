cba8a2796eed39836e5e22b986aa48d239e7adcc
Justin Shih <justinshih@Justins-MacBook-Pro.local>
2018-07-06 18:37:25 -0700

    Changes webpack to production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: path.join(__dirname, '/client/index.jsx'),
   resolve: {
     extensions: ['.js', '.jsx'],
